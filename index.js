4b6ced65c1d8333c384889611b271e9928cc2711
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-05-18 02:20:09 +0600

    mongodob db , services api endpoint added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -9,16 +10,46 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//Mongodb Config
+const uri =
+   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gvs9c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
+});
+
+// API Endpoints
+async function run() {
+   try {
+      await client.connect();
+      const serviceCollections = client
+         .db("doctors_portal_DB")
+         .collection("services");
+
+      //Find All Services
+      app.get("/services", async (req, res) => {
+         const query = {};
+         const cursor = serviceCollections.find(query);
+         const services = await cursor.toArray();
+         res.send(services);
+      });
+   } finally {
+      //   await client.close();
+   }
+}
+run().catch(console.dir);
+
 //API  Endpoints
 app.get("/", (req, res) => {
-    res.send({
-        success: true,
-        message: "hello from doctors portal server",
-        developedBy: "Muhammad Touhiduzzaman",
-    });
+   res.send({
+      success: true,
+      message: "hello from doctors portal server",
+      developedBy: "Muhammad Touhiduzzaman",
+   });
 });
 
 //Listening to port
 app.listen(port, () => {
-    console.log("listening to port:", port);
+   console.log("listening to port:", port);
 });
